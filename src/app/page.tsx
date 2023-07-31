277c734817e66d780e3ab23b82d78c0c004b9bd5
Elle Majors <hellolovely@ellemajors.com>
2023-07-31 15:01:33 -0600

    add: whats happening

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,81 +1,45 @@
-import LeftSidebar from './components/LeftSidebar'
+import LeftSidebar from './components/LeftSidebar';
 import MainComponent from './components/MainComponent';
+import { BsSearch } from 'react-icons/bs';
 
 const Home =() => {
   return (
     <div className="w-full h-full flex justify-center items-center relative bg-black">
-      <div className="max-w-screen-xl w-full h-full flex relative">
-        <LeftSidebar/>
-        <MainComponent/>
-        {/* add fixed to prevent the sidebar from scrolling
-        <main className="ml-[275px] flex w-full max-w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
-          <h1 className="text-xl font-bold p-6 backdrop-blur bg-black/10 sticky top-0">Home</h1>
-          <div className="border-t-[0.5px] px-4 border-b-[0.5px] flex items-stretch py-6 space-x-2 border-gray-600 relative">
-            <div className="w-10 h-10 bg-slate-400 rounded-full flex-none"></div>
-            <div className="flex flex-col w-full h-full">
+      <div className="max-w-[70vw] w-full h-full flex relative">
+        <LeftSidebar />
+        <MainComponent />
+        <section className="w-full sticky top-2 mt-2 flex flex-col items-stretch h-screen px-6">
+          <div>
+            <div className="relative w-full h-full group">
               <input 
+                id="searchBox"
                 type="text"
-                className="w-full h-full placeholder:text-gray-600 bg-transparent border-b-[0.5px] border-gray-600 p-4 outline-none border-none"
-                placeholder="What's happening?"
+                placeholder="Search Twitter" 
+                className="outline-none peer focus:border-primary focus:border bg-neutral-900/90 w-full h-full rounded-xl py-4 pl-14 pr-4"
               />
-              <div className="w-full justify-between items-center flex">
-                <div></div>
-                <div className="w-full max-w-[100px]">
-                  <button className="rounded-full bg-primary px-4 py-2 w-full text-lg text-center hover:bg-opacity-70 transition duration-200 font-bold">
-                  Tweet
-                  </button>
-                </div>
-              </div>
+              <label 
+                htmlFor="searchBox"
+                className="absolute top-0 left-0 h-full flex items-center justify-center p-4 text-gray-500 peer-focus:text-primary"
+              >
+                <BsSearch className="w-5 h-5"/>
+              </label>
             </div>
           </div>
-          <div className="flex flex-col">
-            {Array.from({ length: 5 }).map((_, i) => (
-              <div key={i} className="border-b-[0.5px] border-gray-600 p-4 flex space-x-4" >
-                <div>
-                  <div className="w-10 h-10 bg-slate-200 rounded-full flex-none"></div>
+          <div className="flex flex-col rounded-xl bg-neutral-900 my-4">
+            <h3 className="font-bold text-xl my-4 px-4">What's happening</h3>
+            <div>
+              {Array.from({ length: 5 }).map((_, i) => (
+                <div key={i}
+                  className="hover:bg-white/10 p-4 last:rounded-b-xl transition duration-200"  
+                >
+                  <div className="font-bold text-lg">#trending ${i+1}</div>
+                  <div className="text-xs text-neutral-400">35.4k</div>
                 </div>
-                <div className="flex flex-col">
-                  <div className="flex items-center w-full justify-between">
-                  <div className="flex items-center space-x-1 w-full">
-                  <div className="font-bold">Elle Majors</div>
-                  <div className="text-gray-500">@ellemajors</div>
-                  <div className="text-gray-500">
-                    <BsDot/>
-                  </div>
-                  <div className="text-gray-500">1 hr ago</div>
-                  </div>
-                  <div className="rounded-full hover:bg-white/10 transition p-3 cursor-pointer">
-                    <BsThreeDots/>
-                  </div>
-                  </div>
-                  <div className="text-white text-base">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus quos aliquid molestiae ut laudantium consequatur illum accusantium tempore culpa ullam inventore facere atque in totam, iusto repudiandae? Velit, unde consequatur.
-                    Quae labore perferendis iusto adipisci!
-                  </div>
-                  <div className="bg-slate-400 aspect-square w-full h-80 rounded-xl mt-2"></div>
-                  <div className="flex items-center justify-start space-x-20 mt-2 w-full">
-                      <div className="rounded-full hover:bg-white/10 transition p-3 cursor-pointer">
-                        <BsChat/>
-                      </div>
-                      <div className="rounded-full hover:bg-white/10 transition p-3 cursor-pointer">
-                        <AiOutlineRetweet/>
-                      </div>
-                      <div className="rounded-full hover:bg-white/10 transition p-3 cursor-pointer">
-                        <AiOutlineHeart/>
-                      </div>
-                      <div className="rounded-full hover:bg-white/10 transition p-3 cursor-pointer">
-                        <IoStatsChartOutline/>
-                      </div>
-                      <div className="rounded-full hover:bg-white/10 transition p-3 cursor-pointer">
-                        <IoShareOutline/>
-                      </div>
-                    </div>
-                </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </main> */}
-        {/* <section>right sidebar</section> */}
+          <div></div>
+        </section>
       </div>
     </div>
   );
